4be8185dac449fa518ab5829f5900c7e2d6037af
James6350 <nziokamutua6350@gmail.com>
2023-09-08 04:09:34 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	/*This program prints digists*/
+	for (i = 0; i < 10; i++)
+	{
+		printf ("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
