2c85e08df9d3716d8f3c8b2e2374de57c0ac6bb8
James6350 <nziokamutua6350@gmail.com>
2023-09-08 03:32:54 -0700

    ./3-print_alphabets | cat -e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+	int i, j;
+	
+	letter = 'a';
+
+	for( i = letter; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	letter = 'A';
+	
+	for( j = letter; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
