34d959aee6367b30ebab2d9795fa09cf2c441642
James6350 <nziokamutua6350@gmail.com>
2023-09-12 04:04:45 -0700

    Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*The liblaries I used*/
+#include <stdio.h>
+
+/*Now the prototypes*/
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
